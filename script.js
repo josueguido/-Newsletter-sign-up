7187e4edc904d33f5f7d56e4d4608aabe9a6d062
Your Name <you@example.com>
2023-09-20 18:48:41 -0600

    Fix html

---
+++ script.js
@@ -0,0 +1,25 @@
+function Error() {
+         document.addEventListener("DOMContentLoaded", function () {
+        // Selecciona los elementos relevantes
+        const emailInput = document.getElementById("email");
+        const submitLink = document.getElementById("submit-link");
+        const errorMessage = document.getElementById("error-message");
+    
+        // Agrega un evento de clic al botón
+        submitLink.addEventListener("click", function (event) {
+            // Verifica si el correo electrónico contiene el símbolo "@"
+            if (!emailInput.value.includes("@")) {
+                               // Muestra el mensaje de error y evita que el formulario se envíe
+                               errorMessage.textContent = "Valid Email Required";
+                event.preventDefault();
+            } else {
+                errorMessage.textContent = "";
+                // Redirige al usuario a success.html
+                window.location.href = "./sucess.html";
+            }
+        });
+    });
+}
+
+window.addEventListener('load', Error);
+
